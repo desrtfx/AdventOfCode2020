849007b8e27c54e47a935393da2ae5f91f0a9980
desrtfx <desrtfx@gmail.com>
2020-12-03 21:45:42 +0100

    Modified part 2 to use stream

--- src/day03/Day03.java
+++ src/day03/Day03.java
@@ -4,22 +4,20 @@ import java.awt.Point;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day03 {
-	
+
 	static char[][] data;
 	static int width;
-	
+
 	static {
 		try {
-			data =  Files.readAllLines(Paths.get("Input/Day03.txt"))
+			data = Files.readAllLines(Paths.get("Input/Day03.txt"))
 					    .stream()
 					    .map(e -> e.toCharArray())
 					    .toArray(char[][]::new);
-			width = data[0].length; 
+			width = data[0].length;
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
@@ -28,37 +26,27 @@ public class Day03 {
 
 	static int part01(char[][] data, Point move) {
 		int cnt = 0;
-		Point cur = new Point(0,0);
+		Point cur = new Point(0, 0);
 		while (cur.y < data.length) {
 			if (data[cur.y][cur.x] == '#') {
 				cnt++;
 			}
-			cur.translate(move.x,move.y);
+			cur.translate(move.x, move.y);
 			cur.x = cur.x % width;
 		}
 		return cnt;
 	}
-	
+
 	static long part02(char[][] data) {
-		List<Point> tracks = new ArrayList<>();
-		tracks.add(new Point(1,1));
-		tracks.add(new Point(3,1));
-		tracks.add(new Point(5,1));
-		tracks.add(new Point(7,1));
-		tracks.add(new Point(1,2));
-		
-		long res = 1L;
-		for(Point p : tracks) {
-			res = res * part01(data, p);
-		}
+		long res = Stream.of(new Point(1, 1), new Point(3, 1), new Point(5, 1), new Point(7, 1), new Point(1, 2))
+			           	 .mapToLong(p -> part01(data, p))
+			           	 .reduce((a, b) -> a * b)
+			           	 .getAsLong();
 		return res;
 	}
-	
-	
-	
-	
+
 	public static void main(String[] args) {
-		System.out.println("Part  1: " + part01(data, new Point(3,1)));
+		System.out.println("Part  1: " + part01(data, new Point(3, 1)));
 		System.out.println("Part  2: " + part02(data));
 	}
 }
