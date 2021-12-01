3c4ec2bbb73a3a3dca641d84ff2e3e9a2a661595
desrtfx <desrtfx@gmail.com>
2021-12-01 21:18:44 +0100

    Final commit, day 24, day 18

--- src/day18/Day18.java
+++ src/day18/Day18.java
@@ -1,6 +1,23 @@
 package day18;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Day18 {
+	
+	static List<String> data;
+	
+	static {
+		try {
+			data = Files.readAllLines(Paths.get("Input/Day18.txt"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit((-1));
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/day24/Day24.java
+++ src/day24/Day24.java
@@ -2,6 +2,24 @@ package day24;
 
 public class Day24 {
 
+	static class HexNode {
+		public HexNode east = null;
+		public HexNode southeast = null;
+		public HexNode southwest = null;
+		public HexNode west = null;
+		public HexNode northwest = null;
+		public HexNode northeast = null;
+		char color = 'w';
+		
+		public void flip( ) {
+			color = (color == 'w') ? 'b' : 'w';
+		}
+	}
+	
+	public enum Dirs {E, SE, SW, W, NW, NE };
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
